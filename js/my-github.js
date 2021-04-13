0b3f179539b2680f7f38cf55cb1167de0304f064
codemaoz90 <codemaoz@gmail.com>
2021-04-13 22:30:03 +0200

    Finished Homework

--- js/my-github.js
+++ js/my-github.js
@@ -1,1 +1,34 @@
 // Write code here to communicate with Github
+const getRepos = function(repoName) {
+  return fetch("https://api.github.com/users/" + repoName + "/repos")
+    .then(data => data.json())
+    .then(function(response) {
+      var ul = document.querySelector("#repos-list");
+      return response.map(function(rep) {
+        console.log(rep.name);
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        a.innerText = rep.name;
+        a.href = "https://github.com/" + repoName + "/" + rep.name;
+        li.appendChild(a);
+        ul.appendChild(li);
+        return rep.name;
+      });
+    });
+};
+
+const getNRepos = function(repoName) {
+  return fetch("https://api.github.com/users/" + repoName)
+    .then(data => data.json())
+    .then(function(response) {
+      var a = document.querySelector("#repos-count");
+      a.innerText = response.public_repos;
+      a.href = "https://github.com/" + repoName;
+      console.log("Total repos: " + response.public_repos);
+      return response.public_repos;
+    });
+};
+
+getRepos("codemaoz90");
+getNRepos("codemaoz90");
+console.log("Loading...");
